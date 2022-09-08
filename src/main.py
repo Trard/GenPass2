eafb7476ab739256bb02e6ff2060d382558f2e14
Trard <trardone@gmail.com>
2022-09-08 17:19:00 +0300

    some more understandable imports

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from generator_widget_commands import *
 from main_menu.widget_commands import *
 from check_widget_commands import *
-from set_widget_commands import *
-from title_interface import *
+from set_widget_commands import main_menu_interface, root
+from title_interface import title_interface
 
 title_interface.place()
 main_menu_interface.place()
