69597502802a40f99da5b0ebdc8c57a614f06860
Trard <trardone@gmail.com>
2022-09-08 15:12:53 +0300

    main.py must be main

---
+++ main.py
@@ -0,0 +1,9 @@
+from generator_widg_commands import *
+from mainmenu_widg_commands import *
+from check_widg_commands import *
+from set_widg_commands import *
+from title_interface import *
+
+title_interface.place()
+mmenu_interface.place()
+root.mainloop()
--- main_file.py
+++
@@ -1,9 +0,0 @@
-from generator_widg_commands import *
-from mainmenu_widg_commands import *
-from check_widg_commands import *
-from set_widg_commands import *
-from title_interface import *
-
-title_interface.place()
-mmenu_interface.place()
-root.mainloop()
